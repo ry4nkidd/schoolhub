1c2bd05424558d41d58584cab9d01c9f544682a7
ry4nkidd <132967070+ry4nkidd@users.noreply.github.com>
2025-08-08 04:43:57 -0500

    Update socket.js

--- api/socket.js
+++ api/socket.js
@@ -1,19 +1,7 @@
 
-const express = require('express');
 const { Server } = require('socket.io');
-const { createServer } = require('http');
 
-const app = express();
-const server = createServer(app);
-
-// CORS configuration for Vercel
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
-});
-
+let io;
 const users = [];
 const botName = "ViceChat Bot";
 
@@ -46,47 +34,59 @@ function formatMessage(username, text) {
   };
 }
 
-io.on('connection', (socket) => {
-  socket.on('joinRoom', ({ username, room }) => {
-    const user = userJoin(socket.id, username, room);
-    socket.join(user.room);
-
-    socket.emit('message', formatMessage(botName, 'Welcome to ViceChat!'));
-    socket.broadcast
-      .to(user.room)
-      .emit('message', formatMessage(botName, `${user.username} has joined the chat`));
-
-    io.to(user.room).emit('roomUsers', {
-      room: user.room,
-      users: getRoomUsers(user.room),
-    });
-  });
-
-  socket.on('chatMessage', (msg) => {
-    const user = getCurrentUser(socket.id);
-    if (user) {
-      io.to(user.room).emit('message', formatMessage(user.username, msg));
-    }
-  });
-
-  socket.on('disconnect', () => {
-    const user = userLeave(socket.id);
-    if (user) {
-      io.to(user.room).emit(
-        'message',
-        formatMessage(botName, `${user.username} has left the chat`)
-      );
-      io.to(user.room).emit('roomUsers', {
-        room: user.room,
-        users: getRoomUsers(user.room),
-      });
-    }
-  });
-});
-
 module.exports = (req, res) => {
   if (!res.socket.server.io) {
+    console.log('Initializing Socket.io');
+    
+    io = new Server(res.socket.server, {
+      cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+      }
+    });
+
+    io.on('connection', (socket) => {
+      console.log('User connected:', socket.id);
+
+      socket.on('joinRoom', ({ username, room }) => {
+        const user = userJoin(socket.id, username, room);
+        socket.join(user.room);
+
+        socket.emit('message', formatMessage(botName, 'Welcome to ViceChat!'));
+        socket.broadcast
+          .to(user.room)
+          .emit('message', formatMessage(botName, `${user.username} has joined the chat`));
+
+        io.to(user.room).emit('roomUsers', {
+          room: user.room,
+          users: getRoomUsers(user.room),
+        });
+      });
+
+      socket.on('chatMessage', (msg) => {
+        const user = getCurrentUser(socket.id);
+        if (user) {
+          io.to(user.room).emit('message', formatMessage(user.username, msg));
+        }
+      });
+
+      socket.on('disconnect', () => {
+        const user = userLeave(socket.id);
+        if (user) {
+          io.to(user.room).emit(
+            'message',
+            formatMessage(botName, `${user.username} has left the chat`)
+          );
+          io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room),
+          });
+        }
+      });
+    });
+
     res.socket.server.io = io;
   }
+  
   res.end();
 };
